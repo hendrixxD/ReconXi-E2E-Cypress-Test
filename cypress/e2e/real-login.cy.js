bcc863d45e36e112d9247c589792aab1508fc962
Mitch <ndianguimichelle@gmail.com>
2025-03-29 13:22:43 +0300

    functional login test

--- cypress/e2e/real-login.cy.js
+++ cypress/e2e/real-login.cy.js
@@ -1,12 +1,12 @@
-describe('Google', function () {
-    beforeEach(function () {
+describe('real google authentication', function () {
+    before(function () {
       // cy.task('db:seed')
       // cy.loginByGoogleApi()
-     cy.login()
-    })
-  
+     cy.loginEndpoint()
+    });
+
     it('shows dashboard', function () {
       // cy.contains('Get Started').should('be.visible')
       cy.visit('/dashboard');
     });
-  })
+});
